2f8ac1e6b1065f56ef382fcc2f58e7ec052951ee
Fer <fgarcia@mail.wm.edu>
2019-03-21 14:51:36 -0400

    heroku deployment

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports={
+    mongoURI:"mongodb+srv://fernanda:fer123@list-ypssg.mongodb.net/test?retryWrites=true"
+    
+}
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://fernanda:fer123@list-ypssg.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+const bodyParser = require ('body-parser');
+const items = require ('./routes/API/items');
+const path = require ('path');
+
+const app = express();
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+//DB Config
+
+const db= require('./config/keys').mongoURI;
+
+//Connect to mongoDB
+
+mongoose.connect(db, { useNewUrlParser: true })
+.then(()=>console.log("We're good!"))
+.catch(err=>console.log(err));
+// Use Routes
+
+app.use('/api/items', items);
+
+//Serve static assets if in production
+
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+}
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`server started on port ${port}`));
