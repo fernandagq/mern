c5419da2e61e67edb0d58e2e53251a258f567d44
Fer <fgarcia@mail.wm.edu>
2019-03-24 23:24:04 -0400

    more changes

--- server.js
+++ server.js
@@ -24,16 +24,14 @@ app.use('/api/items', items);
 
 //Serve static assets if in production
 
-if(process.env.NODE_ENV === 'production'){
-    // Set static folder
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    })
-
-}
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`server started on port ${port}`));
